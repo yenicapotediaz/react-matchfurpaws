0615a1d7db8b070f837045f2a2edf2e9cabb5021
Yeni <yenicapote08@gmail.com>
2017-01-21 02:21:14 -0800

    Added all pages to app for rendering.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Profile from './Components/Profile';
+import Pets from './Components/Pets';
+import Login from './Components/Login';
+import Footer from './Components/Footer';
 
 class App extends Component {
-
   render() {
     return (
-      <div>
-
+      <div className="App">
+        <Header />
+        <About />
+        <Profile />
+        <Pets />
+        <Login />
+        <Footer />
       </div>
     );
   }
