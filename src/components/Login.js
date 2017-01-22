5b86ee964316dfddc8aead0596156da2e7696651
Yeni <yenicapote08@gmail.com>
2017-01-22 02:23:27 -0800

    Added second form for allowing a user to create an account. CreateUser form renders after submit button is clicked even with e.preventDefault(); and Login form does not. Hmmm...

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,57 @@
 import React, { Component } from 'react';
 
+class CreateUser extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newUser: {}
+    }
+  }
+
+  handleNewUser(e){
+  this.setState({newUser:{
+    name: this.refs.email.value,
+    email: this.refs.email.value,
+    password: this.refs.password.value,
+    password_confirmation: this.refs.confirm_password.value
+  }}, function(){
+    console.log(this.state.newUser);
+    // this.props.login(this.state.user);
+  });
+  //add a way to clear form
+  e.preventDefault();
+
+}
+
+  render() {
+    return (
+      <div id="create-user">
+        <h3> Or create a new account! </h3>
+        <form onSubmit={this.handleNewUser.bind(this)}>
+        <div>
+          <label>Name</label><br/>
+          <input type="text" ref="name" />
+        </div>
+        <div>
+          <label>Email</label><br/>
+          <input type="text" ref="email" />
+        </div>
+        <div>
+          <label>Password</label><br/>
+          <input type="password" ref="password" />
+        </div>
+        <div>
+          <label>Confirm Password</label><br/>
+          <input type="password" ref="password_confirmation" />
+        </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+
 class Login extends Component {
   constructor() {
     super();
@@ -24,6 +76,7 @@ class Login extends Component {
   render() {
     return (
       <div id="login">
+      <CreateUser />
         <h3> Welcome, please log in! </h3>
         <form onSubmit={this.handleSubmit.bind(this)}>
         <div>
@@ -32,10 +85,11 @@ class Login extends Component {
         </div>
         <div>
           <label>Password</label><br/>
-          <input type="text" ref="password" />
+          <input type="password" ref="password" />
         </div>
           <input type="submit" value="Submit" />
         </form>
+
       </div>
     );
   }
